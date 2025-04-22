8cc280f45f2161e016b9eb7c42ece26accabae6a
mahdi <maknimahdi6@gmail.com>
2025-04-22 16:28:49 +0100

    test3

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig as defineVitestConfig } from 'vitest/config';
 
+
 export default defineConfig({
   plugins: [react()],
   test: {
     setupFiles: './src/setupTests.js', // Assure-toi que le fichier existe
     globals: true, // Optionnel selon tes préférences
+    environment: 'jsdom',
   },
 });
